af35324dc6d121a198e8a0ed305f839a6f66a7bb
szabinalovas <szabinaa.lovas@gmail.com>
2022-02-15 20:56:51 +0100

    implement storeImage

---
+++ src/main/java/com/codecool/fileshare/controller/DatabaseController.java
@@ -0,0 +1,23 @@
+package com.codecool.fileshare.controller;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DatabaseController {
+
+    public DataSource connect() throws SQLException {
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setDatabaseName(System.getenv().get("DATABASE_NAME"));
+        ds.setUser(System.getenv().get("USERNAME"));
+        ds.setPassword(System.getenv().get("PASSWORD"));
+
+        System.out.println("Trying to connect...");
+        ds.getConnection().close();
+        System.out.println("Connection OK");
+
+        return ds;
+    }
+}
+
--- src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
@@ -1,26 +1,74 @@
 package com.codecool.fileshare.repository;
 
+import com.codecool.fileshare.controller.DatabaseController;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @Component("jdbc")
-public class ImageJdbcRepository implements ImageRepository{
+public class ImageJdbcRepository implements ImageRepository {
+    DatabaseController dc = new DatabaseController();
 
-
+    /**
+     * implement store image in database here
+     * content is base64 coded image file
+     * generate and return uuid of stored image
+     * https://www.base64-image.de/
+     * https://codebeautify.org/base64-to-image-converter
+     */
     @Override
     public String storeImage(String category, String content) {
-        // implement store image in database here
-        // content is base64 coded image file
-        // generate and return uuid of stored image
-        // https://www.base64-image.de/
-        // https://codebeautify.org/base64-to-image-converter
+        DataSource ds = null;
+        try {
+            ds = dc.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection conn = ds.getConnection()) {
+            String sql = "INSERT INTO image (category, content, extension) VALUES (?, ?, ?);";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, category);
+            st.setBytes(2, content.getBytes());
+            st.setString(3, "null");
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return getUUIDByCategoryAndContent(category, content, ds);
+    }
+
+    private String getUUIDByCategoryAndContent(String category, String content, DataSource ds) {
+        try (Connection conn = ds.getConnection()) {
+            String uuidSelect = "SELECT id FROM image WHERE category = ? AND content = cast(? AS bytea)";
+            PreparedStatement st = conn.prepareStatement(uuidSelect);
+            st.setString(1, category);
+            st.setString(2, content);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                return rs.getString(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
+    /**
+     * @param uuid
+     * implement readImage from database here
+     * @return base64 encoded image
+     */
     @Override
     public String readImage(String uuid) {
-        // implement readImage from database here
-        // return base64 encoded image
         return null;
     }
 }
